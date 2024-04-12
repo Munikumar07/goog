03fa606af4e1cb067e73d988046047897b8fdf5b
Munikumar07 <161303070+Munikumar07@users.noreply.github.com>
2024-04-12 17:16:19 +0530

    Update js.js

--- js.js
+++ js.js
@@ -72,20 +72,5 @@ sr.reveal('.about-col-1',{delay:400, origin: 'left'});
 sr.reveal('.about-col-2',{delay:600, origin: 'top'});
 sr.reveal('.row', {delay:200, origin: 'bottom'});
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-};
+
 
